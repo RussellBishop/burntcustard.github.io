45c3b2118dee1c244cf649299e23d5babf7fd773
burntcustard <burntcustard@gmail.com>
2020-04-18 22:30:56 +0100

    Testing non-separate posts & pages

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,10 @@ const cleanCSS = require('gulp-clean-css');
 const concat = require('gulp-concat');
 const gzipSize = require('gzip-size');
 const through = require('through2').obj;
-const markdown = require('gulp-markdown');
+const path = require('path');
+//const markdown = require('gulp-markdown');
+const markdown = require('marked');
+const glob = require ('glob');
 
 function clean(cb) {
   return del(['dist/']);
@@ -60,7 +63,7 @@ function js() {
     .pipe(gulp.dest('dist/js'));
 }
 
-function replaceParts(html) {
+function slotParts(html) {
   const regex = /( *)(?:<part src=")([a-zA-Z./-]*)(?:"\/?>)/g;
   const replacer = (match, indent, filename) => {
     try {
@@ -81,7 +84,7 @@ function replaceParts(html) {
   return html.replace(regex, replacer);
 }
 
-function replaceContent(html, content) {
+function slotContent(html, content) {
   const regex = /( *)(?:<content\/?>)/g;
   const replacer = (match, indent) => {
     return content.replace(/^/gm, indent);
@@ -90,35 +93,79 @@ function replaceContent(html, content) {
   return html.replace(regex, replacer);
 }
 
+function setCurrentNav(html, path) {
+  const filename path.basename(path, path.extname(path));
+  const dirname = path.basename(path.dirname(path));
+  const regex = new RegExp(`href="\/${filename}|${dirname}"`);
+  return html.replace(regex, '$& aria-current="page"');
+}
+
+// function html() {
+//   return gulp
+//     .src([
+//       'src/pages/*.html'
+//     ], { base: './src/pages' })
+//     .pipe(through((chunk, encoding, callback) => {
+//       let html = chunk.contents.toString();
+//       let filename = path.basename(chunk.path, path.extname(chunk.path));
+//       html = replaceParts(html);
+//       html = setCurrentNav(html, filename);
+//       chunk.contents = Buffer.from(html);
+//       callback(null, chunk);
+//     }))
+//     .pipe(gulp.dest('./dist'))
+// }
+//
+// function posts() {
+//   let single = fs.readFileSync('src/posts/single.html').toString();
+//   return gulp
+//     .src([
+//       'src/posts/*.md'
+//     ], { base: './src/posts' })
+//     .pipe(markdown())
+//     .pipe(through((chunk, encoding, callback) => {
+//       let content = chunk.contents.toString();
+//       let html = replaceContent(single, content);
+//       html = replaceParts(html);
+//       //html = setCurrentNav(html, filename);
+//       chunk.contents = Buffer.from(html);
+//       callback(null, chunk);
+//     }))
+//     .pipe(gulp.dest('./dist/blog'))
+// }
+
 function html() {
+  let templates = {};
+  [...glob.sync('.src/*/template.html')].forEach(filename => {
+    templates[filename] = fs.readFileSync(filepath).toString();
+  });
   return gulp
     .src([
-      'src/pages/*.html'
-    ], { base: './src/pages' })
+      'src/**/*.html',
+      '!src/parts/', // Exclude the parts folder
+      '!src/**/template.html', // Exclude template files
+      'src/**/*.md'
+    ], { base: './src/' })
     .pipe(through((chunk, encoding, callback) => {
-      let html = chunk.contents.toString();
-      html = replaceParts(html);
+      let content = chunk.contents.toString();
+
+      // If the file is markdown, convert it to HTML
+      if (chunk.path.extname === '.md') {
+        content = markdown(content);
+      }
+
+      // If the file is in a folder with a template, slot the content in it
+      if (chunk.path.dirname + '/template.html' in templates) {
+        slotContent(templates[chunk.path.dirname + '/template.html'], content);
+      }
+
+      content = slotParts(content);
+      content = setCurrentNav(content, chunk.path);
+
       chunk.contents = Buffer.from(html);
       callback(null, chunk);
     }))
     .pipe(gulp.dest('./dist'))
-}
-
-function posts() {
-  let single = fs.readFileSync('src/posts/single.html').toString();
-  return gulp
-    .src([
-      'src/posts/*.md'
-    ], { base: './src/posts' })
-    .pipe(markdown())
-    .pipe(through((chunk, encoding, callback) => {
-      let content = chunk.contents.toString();
-      let html = replaceContent(single, content);
-      html = replaceParts(html);
-      chunk.contents = Buffer.from(html);
-      callback(null, chunk);
-    }))
-    .pipe(gulp.dest('./dist/blog'))
 }
 
 exports.clean = clean;
