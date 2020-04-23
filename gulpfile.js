d50b1d0bcd6f2b23c0fb905d96d9c19728f06087
burntcustard <burntcustard@gmail.com>
2020-04-23 03:06:00 +0100

    Testing some things for new site design

--- gulpfile.js
+++ gulpfile.js
@@ -117,6 +117,11 @@ function slotContent(html, content) {
 function setCurrentNav(html, pagePath) {
   const filename = path.basename(pagePath, path.extname(pagePath));
   const dirname = path.basename(path.dirname(pagePath));
+
+  if (dirname === 'pages' && filename === 'index') {
+    return html.replace('href="/"', '$& aria-current="page"');
+  }
+
   const regex = new RegExp(`href="\/${filename}|${dirname}"`);
   return html.replace(regex, '$& aria-current="page"');
 }
